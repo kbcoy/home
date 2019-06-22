5639dc41aa67201ab578ede1da7da20e42becb44
kbcoy <knbpham@gmail.com>
2019-06-22 12:43:58 -0700

    shell empty

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cat ./templates/top.html ./content/index.html ./templates/bottom.html > ./docs/index.html
-cat ./templates/top.html ./content/contact.html ./templates/bottom.html > ./docs/contact.html
-cat ./templates/top.html ./content/toolset.html ./templates/bottom.html > ./docs/toolset.html
-
-echo done
